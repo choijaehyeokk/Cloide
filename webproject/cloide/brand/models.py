1c5fc12fc0a991e128cd15dd1666d240f225e721
jay <enlqn1010@gmail.com>
2020-09-16 23:12:47 +0900

    version7

---
+++ webproject/cloide/brand/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+# Create your models here.
+class Brand(models.Model):
+    name = models.CharField(max_length= 100)
+    bnum = models.IntegerField(primary_key=True)
+    intro = models.TextField()
+    url = models.TextField(max_length = 200)
+    date = models.DateTimeField()
+    logo = models.ImageField(blank = True, upload_to = "images",null = True)
+    phone = models.CharField(max_length = 50)
+
+    def __str__(self):
+        return self.name
+
+
